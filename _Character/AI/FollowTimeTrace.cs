a65a542b769e179b7102d3656994e13d687cc07b
Emil Burman <emil.burman@hotmail.com>
2018-01-12 18:56:33 +0100

    Further cleanup, actionstate interface added

---
+++ _Character/AI/FollowTimeTrace.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTimeTrace : MonoBehaviour
+{
+    // Set the tracing GameObject, fetch its time controll script, fetch its position array and its interpolation value
+    public GameObject traceEntity;
+    ITimeControll timeControllScript;
+    float interpolation;
+
+    void Start()
+    {
+        traceEntity = transform.parent.gameObject;
+        transform.position = transform.parent.position;
+        timeControllScript = traceEntity.GetComponent<ITimeControll>();
+        StartCoroutine(LateStart(2));
+    }
+
+
+    void LateUpdate()
+    {
+        transform.parent = null;
+        transform.position = Vector2.Lerp(transform.position, timeControllScript.GetPositionFromArrayAt(0), 0.09f);
+
+    }
+    IEnumerator LateStart(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        transform.position = timeControllScript.GetPositionFromArrayAt(0);
+    }
+}
---
+++ _Character/StateHandlers/ActionStates/IActionState.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IActionState
+{
+    bool Attacking();
+    bool Dashing();
+    bool Jumping();
+    bool Idle();
+    bool Moving();
+    bool Sprinting();
+    bool ReversingTime();
+    bool Crouching();
+}
